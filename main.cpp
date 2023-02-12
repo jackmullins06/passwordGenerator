a51ee4dd10c29def02a1593390bacfd171cdd654
Jack David Mullins <jackmullins06@gmail.com>
2023-02-12 17:06:53 +0000

    Commit

--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@
 #include <chrono>
 
 /**
- * This function returns a 20 character randomly generated password that has 
- * been generated using a Mersenne Twister pseudo-random generator.
-*/
+ * This function returns a 20 character randomly generated string that has 
+ * been generated using a Mersenne Twister pseudo-random generator, that 
+ * that could be used as a password. */
 std::string password_gen(std::size_t length = 20) {
-    std::string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    if(length < 1) {
+       std::cout <<  "" << std::endl;
+    }
+    
+    std::string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#€£$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
     std::mt19937 generator;
     generator.seed(std::chrono::system_clock::now().time_since_epoch().count());
     std::uniform_int_distribution<std::size_t> distribution(0, characters.size() - 1);
@@ -23,11 +27,10 @@ std::string password_gen(std::size_t length = 20) {
 
 /**
  * The main function prints out 5 different randomly generated passwords
- * to the console.
-*/
+ * to the console. */
 int main() {
-    for(int i = 0; i < 5; ++i) {
-        std::cout << password_gen() << "\n"<< std::endl;
+    for(int i = 0; i < 5; i++) {
+       std::cout << password_gen(35) << "\n" << std::endl;
     }
     return 0;
 }
