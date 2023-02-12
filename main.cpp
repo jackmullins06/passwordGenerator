103bc539d7107aa8ae2be3d398646210d8c7d5cb
Jack David Mullins <jackmullins06@gmail.com>
2023-02-12 00:17:30 +0000

    Added additional comments.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <random>
 #include <chrono>
 
+/**
+ * This function returns a 20 character randomly generated password that has 
+ * been generated using a Mersenne Twister pseudo-random generator.
+*/
 std::string password_gen(std::size_t length = 20) {
     std::string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
     std::mt19937 generator;
@@ -17,6 +21,10 @@ std::string password_gen(std::size_t length = 20) {
     return password;
 }
 
+/**
+ * The main function prints out 5 different randomly generated passwords
+ * to the console.
+*/
 int main() {
     for(int i = 0; i < 5; ++i) {
         std::cout << password_gen() << "\n"<< std::endl;
